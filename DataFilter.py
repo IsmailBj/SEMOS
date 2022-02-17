dbd04e99d849c483bad8dece2309b2404719d0b3
ismo.bjr <ismo.bjr@gmail.com>
2022-02-17 12:00:04 +0100

    commit

--- DataFilter.py
+++ DataFilter.py
@@ -1,5 +1,9 @@
 import sqlite3
+from threading import Thread
+import time
 import pandas as pd
+from multiprocessing import Process
+
 
 class CleanCompanyNames:
 
@@ -7,9 +11,12 @@ class CleanCompanyNames:
         self.con = None
 
     def connect_to_db(self):
+
+
         self.con = sqlite3.connect("semos_company_names.db")
 
     def purify_data(self):
+
 
         self.connect_to_db()
 
@@ -32,19 +39,100 @@ class CleanCompanyNames:
             .replace({'\(.*': ''}, regex=True) \
             .replace({'\s+': ' '}, regex=True)
 
+
+        df['company_name_cleaned'] = df['company_name_cleaned'].str.title()
+        # with dataframe to sql
         df.to_sql('companies', self.con, chunksize=1000, if_exists='replace', index=False)
 
+        # with SQL update queries
+        # return df
 
+    def update_sql(self, df1):
+ 
 
-    def run_program(self):
+        self.connect_to_db()
+        for row in df1:
+            command = '''UPDATE companies SET company_name_cleaned = ? WHERE name = ?'''
+            cursor = self.con.cursor()
+            cursor.execute(command, [row[2], row[1]])
+            self.con.commit()
+        self.con.close()
 
+    def run_program_df(self):
+
+        start = time.perf_counter()
         self.purify_data()
-        print(f"\nPurification of big data finished")
+        finish = time.perf_counter()
+        print(f"\nPurification of big data finished in {round(finish - start, 2)} seconds.\n")
+
+    def run_program_sql(self):
 
 
+        start = time.perf_counter()
+        ddf = self.purify_data()
+        n = [i for i in range(0, 25000, 5000)]
+        for ind in range(0, len(n)-1):
+            self.update_sql(ddf.values[n[ind]:n[ind+1]])
+        finish = time.perf_counter()
+        print(f"\nPurification of big data finished in {round(finish - start, 2)} seconds.\n")
+
+    def with_multiprocessing_df(self):
 
 
-    def print_list(self):
+        start = time.perf_counter()
+        p = Process(target=self.purify_data)
+        p.start()
+        p.join()
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the multiprocessing library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
+
+    def with_multiprocessing_sql(self):
+
+        start = time.perf_counter()
+        ddf = self.purify_data()
+        n = [i for i in range(0, 25000, 5000)]
+        p = dict()
+        for ind in range(0, len(n)-1):
+            print(f'{ind+1} process starts\n')
+            p[ind+1] = Process(target=self.update_sql(ddf.values[n[ind]:n[ind+1]]))
+            p[ind+1].start()
+            p[ind+1].join()
+            print(f'{ind+1}. process ended\n')
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the multiprocessing library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
+
+    def with_threading_df(self):
+
+        start = time.perf_counter()
+        t = Thread(target=self.purify_data)
+        t.start()
+        t.join()
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the threading library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
+
+    def with_threading_sql(self):
+
+        start = time.perf_counter()
+        ddf = self.purify_data()
+
+        n = [i for i in range(0, 25000, 5000)]
+
+        t = dict()
+        for ind in range(0, len(n)-1):
+            print(f'{ind+1} thread starts\n')
+            t[ind+1] = Thread(target=self.update_sql(ddf.values[n[ind]:n[ind+1]]))
+            t[ind+1].start()
+            t[ind+1].join()
+            print(f'{ind+1} thread ended\n')
+        finish = time.perf_counter()
+        print(f'\nPurification of big data with the threading library '
+              f'finished in {round(finish - start, 2)} seconds.\n')
+
+    def print_rows(self):
+
 
         self.connect_to_db()
 
@@ -58,6 +146,12 @@ class CleanCompanyNames:
 
 
 def main():
-    ccn = CleanCompanyNames()
-    ccn.run_program()
-    ccn.print_list()
+
+    # ccn = CleanCompanyNames()
+    # ccn.run_program_df()
+    # ccn.run_program_sql()
+    # ccn.with_multiprocessing_df()
+    # ccn.with_multiprocessing_sql()
+    # ccn.with_threading_df()
+    # ccn.with_threading_sql()
+    # ccn.print_rows()
